bcae45dde733e9ae9832e78cd9559e0caee57abc
Nara Macias <116755949+Lilnnara@users.noreply.github.com>
2024-04-05 17:48:34 -0600

    Added Car Class Attribures (#6)

--- Car.java
+++ Car.java
@@ -1,7 +1,20 @@
 public class Car {
+    private String id;
+    private String carType;
+    private String model;
+    private String condition;
+    private String color;
+    private int capacity;
+    private double mileage;
+    private String fuelType;
+    private String transmission;
+    private String vin;
+    private double price;
+    private int carsAvailable;
+
     //carInformation.split(",")[*] to use the userInfo values as arrays 
     //or temp = carInformation.split(","); temp[*]
     public Car(String carInformationIn){
-        
-    }
+
+    }
 }
