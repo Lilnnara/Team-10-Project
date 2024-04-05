1efbbf96a3532554a9ad144d59abf48f23d08d9f
“Jenna <158506727+JennaMurphree@users.noreply.github.com>
2024-04-05 06:12:58 -0600

    Updated Log class with attributes, a constructor, and a toString() method.  Constructor still needs generate current time and date function, to be complete. - Jenna

--- Log.java
+++ Log.java
@@ -1,3 +1,24 @@
 public class Log {
-    
+    private String timestamp; 
+    private String eventDescription; 
+    private String Username;
+
+    /** Constructs a new Log entry with the supplied eventDescriptionIn and UsernameIn using the current timre and date value. */
+    public Log(String eventDescriptionIn, String UsernameIn){
+        //generate current date and time and assign as a string to timestamp
+        timestamp = "temp";
+        eventDescription = eventDescriptionIn;
+        Username = UsernameIn;
+    }
+
+    //insert Getters and Setters here
+
+    /** 
+     * This method generates a String value of all the concatenated attributes of the Log file.
+     * 
+     * @return String value of all Log attributes
+     */
+    public String toString(){
+        return timestamp+eventDescription+Username;
+    }
 }
