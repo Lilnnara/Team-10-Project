9318710735257b9daad56c353c868e7b1f973e96
Nara Macias <116755949+Lilnnara@users.noreply.github.com>
2024-04-05 17:44:25 -0600

    Update log and build initialize shop (#5)
    * Updated Log class with attributes, a constructor, and a toString() method.  Constructor still needs generate current time and date function, to be complete. - Jenna
    
    * Added initializeShop() and readFile() to the RunShop Class.  Added temporary constructors to Car and User classes so that the initializeShop method could create Car and User objects with a String and not cause Errors.  Initializing Objects with a String is a process that can be changed later or implemented fully. - Jenna
    
    * Wrote current date/time value code in Log file and added comments explaining how to use the constructors with only a string. - Jenna
    
    * Update Car.java
    
    ---------
    
    Co-authored-by: “Jenna <158506727+JennaMurphree@users.noreply.github.com>

--- Car.java
+++ Car.java
@@ -1,5 +1,7 @@
 public class Car {
+    //carInformation.split(",")[*] to use the userInfo values as arrays 
+    //or temp = carInformation.split(","); temp[*]
     public Car(String carInformationIn){
-        
-    }
+        
+    }
 }
